323efa346ac9144360fd14bd939692c94c5646d9
newgamerhb <heerokbiswas457@gmail.com>
2026-09-05 21:08:39 +0600

    .

--- borrowed-time/Scripts/Touch_Controls.gd
+++ borrowed-time/Scripts/Touch_Controls.gd
@@ -1,8 +1,14 @@
 extends CanvasLayer
 
 @onready var buttons: Array[TouchScreenButton] = [$Left, $Right, $Jump]
+@onready var pause: TouchScreenButton = $Pause
 
 func _ready() -> void:
 	for btn in buttons:
 		btn.pressed.connect(func(): btn.modulate.a = 0.5)
 		btn.released.connect(func(): btn.modulate.a = 1.0)
+
+
+@onready var pause_menu: CanvasLayer = $"../Pause_Menu"
+func _on_pause_released() -> void:
+	pause_menu.pause()
