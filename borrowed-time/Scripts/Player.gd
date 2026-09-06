87875c01359b43eea14e120441cd8bcb9b9a1fb9
newgamerhb <heerokbiswas457@gmail.com>
2026-09-06 11:35:21 +0600

    add jump sounds

--- borrowed-time/Scripts/Player.gd
+++ borrowed-time/Scripts/Player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var sfx_jump: AudioStreamPlayer = $sfx_jump
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -13,9 +14,9 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("move_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		sfx_jump.play()
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
