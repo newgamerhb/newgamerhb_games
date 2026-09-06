65d8396e671360cebf0246e61a3fed3f451500a1
newgamerhb <heerokbiswas457@gmail.com>
2026-09-06 10:48:20 +0600

    fix TS pause

--- borrowed-time/Scripts/Touch_Controls.gd
+++ borrowed-time/Scripts/Touch_Controls.gd
@@ -1,14 +1,15 @@
 extends CanvasLayer
 
+
 @onready var buttons: Array[TouchScreenButton] = [$Left, $Right, $Jump]
-@onready var pause: TouchScreenButton = $Pause
 
 func _ready() -> void:
 	for btn in buttons:
 		btn.pressed.connect(func(): btn.modulate.a = 0.5)
 		btn.released.connect(func(): btn.modulate.a = 1.0)
 
-
 @onready var pause_menu: CanvasLayer = $"../Pause_Menu"
-func _on_pause_released() -> void:
-	pause_menu.pause()
+@onready var pause: TouchScreenButton = $Pause
+func _on_pause_pressed() -> void:
+	get_tree().paused = true
+	pause_menu.visible = true
