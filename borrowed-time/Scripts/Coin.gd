7c4c32424a0235cd9fdf434a11ae8b577e745dac
newgamerhb <heerokbiswas457@gmail.com>
2026-09-08 21:57:06 +0600

    texture change

--- borrowed-time/Scripts/Coin.gd
+++ borrowed-time/Scripts/Coin.gd
@@ -1,12 +1,9 @@
 extends Area2D
 
-func _on_body_entered(_body : Node2D) -> void:
-	_handle_coin_collected.call_deferred()
 
-func  _handle_coin_collected() -> void:
-	# Assumes the body is player
-	# 1. Remove the coin
+func _on_body_entered(_play: Node2D) -> void:
+	_handle_coin_collected()
+
+func _handle_coin_collected() -> void:
 	queue_free()
-	# 2. Change to win screen
-	get_tree().change_scene_to_file("res://Scenes/Win_Screen.tscn")
-	
+	get_tree().change_scene_to_file("res://Scenes/Title_Screen.tscn")
--- borrowed-time/Scripts/Player.gd
+++ borrowed-time/Scripts/Player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 @onready var sfx_jump: AudioStreamPlayer = $sfx_jump
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 300.0
+const  FRICTION = 800.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
 
@@ -16,11 +18,26 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 		sfx_jump.play()
 
-	# Get the input direction and handle the movement/deceleration.
+	# Get the input direction and handle the movement/deceleration, -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
+	
+	# flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# action animetion
+	if direction == 0:
+		animated_sprite.play("Idle")
+	else:
+		animated_sprite.play("Walk")
+	
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
 	move_and_slide()
