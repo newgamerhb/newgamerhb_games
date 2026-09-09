bbd97463f4927143a19d23d3583f6beb94539c39
newgamerhb <heerokbiswas457@gmail.com>
2026-09-10 01:08:03 +0600

    got mad at animations

--- borrowed-time/Scripts/Coin.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-func _on_body_entered(_play: Node2D) -> void:
-	_handle_coin_collected()
-
-func _handle_coin_collected() -> void:
-	queue_free()
-	get_tree().change_scene_to_file("res://Scenes/Title_Screen.tscn")
--- borrowed-time/Scripts/Player.gd
+++ borrowed-time/Scripts/Player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
-@onready var sfx_jump: AudioStreamPlayer = $sfx_jump
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sfx_jump: AudioStreamPlayer = $sfx/sfx_jump
 
 const  FRICTION = 800.0
 const SPEED = 150.0
@@ -20,21 +19,6 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration, -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
-	
-	# flip the sprite
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-	
-	# action animetion
-	if direction == 0:
-		animated_sprite.play("Idle")
-	else:
-		animated_sprite.play("Walk")
-	
-	
-	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
